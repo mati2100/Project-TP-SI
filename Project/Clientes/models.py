55913ffeea758e9683399e8b8966b33d072b041a
adem <ademdjabali10@gmail.com>
2026-01-23 18:00:41 +0100

    added form and delete confrim for all apps except services

--- Project/Clientes/models.py
+++ Project/Clientes/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 class Client(models.Model):
-    client_lastname = models.CharField(max_length=100, verbose_name="Nom")
-    client_firstname = models.CharField(max_length=100, verbose_name="Prénom")
-    client_email = models.EmailField(verbose_name="Adresse email",unique=True)
-    client_phone = models.CharField(max_length=20, verbose_name="Téléphone")
-    client_address = models.TextField(verbose_name="Adresse complète")
-    client_city = models.CharField(max_length=100, verbose_name="Ville")
-    client_country = models.CharField(max_length=100, verbose_name="Pays", default="Algérie")
+    client_lastname = models.CharField(max_length=100, verbose_name="Last Name")
+    client_firstname = models.CharField(max_length=100, verbose_name="First Name")
+    client_email = models.EmailField(verbose_name="Email Address",unique=True)
+    client_phone = models.CharField(max_length=20, verbose_name="Phone Number")
+    client_address = models.TextField(verbose_name="Full Address")
+    client_city = models.CharField(max_length=100, verbose_name="City")
+    client_country = models.CharField(max_length=100, verbose_name="Country", default="Algeria")
     client_registration_date = models.DateTimeField(auto_now_add=True)
     client_actif = models.BooleanField(default=True)
     notes = models.TextField(blank=True, null=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return f"{self.client_lastname} {self.client_firstname} - {'Actif' if self.client_actif else 'Inactif'}"
+        return f"{self.client_lastname} {self.client_firstname} - {'Active' if self.client_actif else 'Inactive'}"
 
     # Méthode pour récupérer les expéditions du client
     def get_shipments(self):
