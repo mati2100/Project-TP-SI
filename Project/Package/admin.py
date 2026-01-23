eb7ec801b724e51d986cc190ff3c895b2e73c941
Ahmed Mati <mahermati459@gmail.com>
2026-01-23 20:28:28 +0100

    Bug Fixes

--- Project/Package/admin.py
+++ Project/Package/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from.models import DeliveryTour
+from.models import Package
 # Register your models here.
-admin.site.register(DeliveryTour)
+admin.site.register(Package)
--- Project/Reclaims/admin.py
+++ Project/Reclaims/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from.models import Complaint
+from.models import Reclaim
 # Register your models here.
-admin.site.register(Complaint)
+admin.site.register(Reclaim)
