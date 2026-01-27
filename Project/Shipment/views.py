15179995b0c5de814ad4cd136791a2f59bdae825
Ahmed Mati <mahermati459@gmail.com>
2026-01-27 22:24:26 +0100

    add style to add new button

--- Project/Shipment/views.py
+++ Project/Shipment/views.py
@@ -42,7 +42,7 @@ def ShipmentCreateView(request):
                 next_url = request.path
 
                 field.help_text = (
-                    f'<button href="{create_url}?next={next_url}">+ Add new</button>'
+                    f'<button class="add-new-button" href="{create_url}?next={next_url}">+ Add new</button>'
                 )
             except:
                 pass
